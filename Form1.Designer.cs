c51af1d7b8f38df8b18340aaae9f70a668107030
Carlos200456 <carlos@cartech.com.ar>
2022-03-29 11:56:29 -0300

    Creacion de CaptureXA simulador de Nano_IF con COM_0_COM

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CaptureXA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonFluoro = new System.Windows.Forms.Button();
+            this.buttonCine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFluoro
+            // 
+            this.buttonFluoro.Location = new System.Drawing.Point(12, 12);
+            this.buttonFluoro.Name = "buttonFluoro";
+            this.buttonFluoro.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonFluoro.Size = new System.Drawing.Size(76, 39);
+            this.buttonFluoro.TabIndex = 55;
+            this.buttonFluoro.Text = "Fluoro";
+            this.buttonFluoro.UseVisualStyleBackColor = true;
+            this.buttonFluoro.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonFLOn_MouseDw);
+            this.buttonFluoro.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonFLOn_MouseUp);
+            // 
+            // buttonCine
+            // 
+            this.buttonCine.Location = new System.Drawing.Point(94, 12);
+            this.buttonCine.Name = "buttonCine";
+            this.buttonCine.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonCine.Size = new System.Drawing.Size(76, 39);
+            this.buttonCine.TabIndex = 56;
+            this.buttonCine.Text = "Cine";
+            this.buttonCine.UseVisualStyleBackColor = true;
+            this.buttonCine.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonCine_MouseDw);
+            this.buttonCine.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonCine_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 66);
+            this.Controls.Add(this.buttonCine);
+            this.Controls.Add(this.buttonFluoro);
+            this.Location = new System.Drawing.Point(1600, 0);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.CaptureXA_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonFluoro;
+        private System.Windows.Forms.Button buttonCine;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureXA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CaptureXA_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            try
+            {
+                serialPort1.PortName = "COM3";
+                serialPort1.BaudRate = int.Parse("57600");
+                serialPort1.DataBits = int.Parse("8");
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "One");
+                serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), "None");
+                serialPort1.Encoding = Encoding.GetEncoding("iso-8859-1");
+                // Encoding = Encoding.GetEncoding("Windows-1252");
+                serialPort1.Open();
+             }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void buttonFLOn_MouseDw(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine("FluoroOn\r");
+                buttonFluoro.BackColor = Color.Yellow;
+            }
+        }
+
+        private void buttonFLOn_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine("FluoroOff\r");
+                buttonFluoro.BackColor = Color.LightGray;
+            }
+
+        }
+
+        private void buttonCine_MouseDw(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine("CineOn\r");
+                buttonCine.BackColor = Color.Yellow;
+            }
+        }
+
+        private void buttonCine_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine("CineOff\r");
+                buttonCine.BackColor = Color.LightGray;
+            }
+        }
+
+    }
+}
